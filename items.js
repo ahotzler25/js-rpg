c80c111c62246e7d33c784093da5ba913b29bd7e
Andy Hotzler <ahotzler@gmail>
2020-12-20 00:53:48 -0600

    Created items.js file and working through items array construction

---
+++ items.js
@@ -0,0 +1,20 @@
+const items = [
+    swords = 
+        { name: "Sword of the Forgotten", attack: 6, defense: 2 },
+        { name: "Blood Pact", attack: 5, defense: 3 },
+        { name: "The Unforgiven", attack: 4, defense: 4},
+    shields = 
+        { name: "Braum's Presence", attack: 1, defense: 7 },
+        { name: "Shield of Perseverance", attack: 2, defense: 5}
+];
+
+function getItems(items) {
+    console.log(items[0].name);
+    console.log(items[1].name);
+}
+
+getItems(items);
+
+
+let result = 0;
+console.log(result);
